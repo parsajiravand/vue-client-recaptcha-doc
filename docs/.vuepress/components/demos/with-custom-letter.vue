3d3d0a228486c4db4cf89dfdfef24cd7d5435402
parsa jiravand <parsajiravand@gmail.com>
2022-12-02 18:23:59 +0330

    add sample with custom letters

--- docs/.vuepress/components/demos/with-custom-letter.vue
+++ docs/.vuepress/components/demos/with-custom-letter.vue
@@ -1,22 +1,32 @@
 <template>
   <div class="sample-captcha">
-    <input type="text" v-model="inputValue" />
-    <!-- Hide CapitalCase And Number And Set Custom Carachters -->
-    <!-- Set 10 Charachter -->
-    <VueClientRecaptcha
-      :value="inputValue"
-      chars="!@#$%^&*"
-      :count="10"
-      @getCode="getCaptchaCode"
-      @isValid="checkValidCaptcha"
-    />
+    <section class="data-box">
+      <input
+        v-model="inputValue"
+        placeholder="submit your captcha"
+        class="input"
+        type="text"
+      />
+
+      <pre class="data">{{ data }}</pre>
+    </section>
+
+    <section class="captcha-box">
+      <!-- Hide CapitalCase And Number And Set Custom Carachters -->
+      <!-- Set 10 Charachter -->
+      <VueClientRecaptcha
+        :value="inputValue"
+        chars="!@#$%^&*"
+        :count="10"
+        @getCode="getCaptchaCode"
+        @isValid="checkValidCaptcha"
+      />
+    </section>
   </div>
 </template>
-
 <script>
-import { ref } from "vue";
+import { ref, reactive } from "@vue/reactivity";
 import VueClientRecaptcha from "vue-client-recaptcha";
-import "vue-client-recaptcha/dist/style.css";
 export default {
   components: {
     VueClientRecaptcha,
@@ -25,19 +35,59 @@ export default {
     /* pass value to captcha  */
     const inputValue = ref(null);
 
+    const data = reactive({
+      captchaCode: null,
+      isValid: false,
+    });
     const getCaptchaCode = (value) => {
       /* you can access captcha code */
-      console.log(value);
+      data.captchaCode = value;
     };
     const checkValidCaptcha = (value) => {
       /* expected return boolean if your value and captcha code are same return True otherwise return False */
-      console.log(value);
+      data.isValid = value;
+      if (value) {
+        alert("Your Captcha is valid now you can submit");
+      }
     };
     return {
       inputValue,
+      data,
       getCaptchaCode,
       checkValidCaptcha,
     };
   },
 };
 </script>
+<style>
+@import url("https://unpkg.com/vue-client-recaptcha/dist/style.css");
+.sample-captcha {
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+}
+.sample-captcha .data-box .input {
+  padding: 10px;
+  border: 1px solid #f1f1f1;
+  border-radius: 4px;
+  width: 100%;
+}
+.sample-captcha .data-box .input:focus {
+  background: #f0f0f00d;
+  outline: none;
+  box-shadow: inset 0 -2px 0 #0077ff;
+}
+.sample-captcha .data-box .data {
+  width: 100%;
+  margin-top: 10px;
+  padding: 20px 0 20px 20px;
+  background-color: #ebebeb;
+  border-radius: 4px;
+}
+.sample-captcha .captcha-box {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin: 10px auto;
+}
+</style>
